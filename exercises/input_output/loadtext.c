43861cf7877e2ccfb0a71eaa99a3491fbdc56f61
peter <peterbaek56@gmail.com>
2021-06-28 23:05:23 +0200

    Exercises

---
+++ exercises/input_output/loadtext.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc,char** argv){
+
+	double x;
+	int items;
+	if(argc < 3){
+	printf("no argument\n");
+	return 0;}
+
+	FILE* my_input_stream = fopen(argv[1],"r");
+	FILE* my_out_stream = fopen(argv[2],"w");
+
+	do  {
+		items = fscanf(my_input_stream, "%lg",&x);
+		if(items != EOF){
+			printf("x = %lg, cos(x) = %lg\n",x, cos(x));
+			fprintf(my_out_stream,"x=%lg, cos(x) = %lg\n",x,cos(x));
+			}
+	} while(items != EOF);
+	fclose(my_input_stream);
+	fclose(my_out_stream);
+
+	return 0;
+
+}
--- exercises/plots/main.c
+++ exercises/plots/main.c
@@ -1,14 +1,30 @@
+
 #include<stdio.h>
 #include<math.h>
 #include<gsl/gsl_sf_erf.h>
+#include<gsl/gsl_sf_gamma.h>
 
-double Erf(double);
+double mygam(double x){
+	if(x<0) return M_PI/sin(M_PI*x)/mygam(1-x);
+	if(x<9) return mygam(x+1)/x;
 
-int main(){
+	double lnGamma= x*log(x+1/(12*x-1/x/10))-x+log(2*M_PI/x)/2;
+	return exp(lnGamma);
+}
 
-	double xmin = -4, xmax =4;	for(double x=xmin;x<=xmax;x+=1.0/8){
-	printf("%10g %10g %10g %10g %10g\n",x,erf(x),gsl_sf_erf(x),Erf(x),tgamma(x));
+
+double myerf(double x){
+	if(x<0) return -myerf(-x);
+	double a[]={0.254829592, -0.284496736, 1.421413741, -1.453152027, 1.061405429};
+	double t=1/(1+0.3275911*x);
+	double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));
+	return 1-sum*exp(-x*x);
+}
+
+int main() {
+	double xmin=0.1, xmax=3;
+	for(double x=xmin; x<=xmax; x+=1.0/8){
+		printf("%10g %10g %10g %10g %10g %10g %10g\n",x,erf(x),gsl_sf_erf(x),myerf(x),tgamma(x),gsl_sf_gamma(x),mygam(x));
 	}
-
 return 0;
 }
